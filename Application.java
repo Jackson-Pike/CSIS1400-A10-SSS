20d5edf611183801cb0da82219a2eaccd1c84e7e
Jackson-Pike <jacksonmpike@Gmail.com>
2019-11-12 15:32:26 -0700

    Initial Setup - .java files
    Just setting up all the classes for the master

---
+++ Application.java
@@ -0,0 +1,14 @@
+/********************************
+** A10 - Team Assignment - Main Application Class
+** Project Description: 
+********************************/
+
+public class Application {
+
+   public static void main(String[] args) {
+      //TO-DO: Impliment main method
+   
+   }
+
+
+}
