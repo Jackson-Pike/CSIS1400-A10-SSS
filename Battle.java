8e5acf69b794cfb178b730cd9866c46da4baa261
AmritShah <57874116+AmritShah@users.noreply.github.com>
2019-11-17 12:31:26 -0700

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -4,16 +4,24 @@
 **
 */
 /*********************************************/
-public class Battle {
-
-   // Fields
-   
-   
-   // Constructors
-   
-   
-   //Methods
-
-
-
+public class Battle
+{
+   String swordName;
+   int swordDmg;
+   public void setName(String n)
+   {
+      swordName = n;
+   }
+   public String getSwordName()
+   {
+      return swordName;
+   }
+   public void setSwordDmg(int d)
+   {
+      swordDmg = d;
+   }
+   public int getSwordDmg()
+   {
+      return swordDmg;
+   }
 }
