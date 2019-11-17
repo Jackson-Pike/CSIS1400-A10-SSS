74f55b12dd0e7db013344d964b3ca8f72382a97b
Austin F <Austin.Fashimpaur@gmail.com>
2019-11-16 23:43:03 -0700

    Update Magic.java

--- Magic.java
+++ Magic.java
@@ -1,9 +1,8 @@
-/**********************************************/
-/*
-**
-**
-*/
-/*********************************************/
+/*****************************************
+* Author : Austin Fashimpaur
+* Date : 11/14/2019
+* Assignment: Magic
+******************************************/
 public class Magic {
 
    // Fields
@@ -13,7 +12,22 @@ public class Magic {
    
    
    //Methods
-
-
-
+      //setters
+   public static void setHealthSpell(int health) {
+      Player.setPlayerHealth(health);
+      //message handler?
+   }
+   
+   public static void setDamageBuffSpell(int damage) {
+      //go to player and set damage buff
+   }
+   
+   public static void setDefenseBuff(int defBuff) {
+      //go to player and set defense buff
+   }
+   
+   public static void setMovementSpell(int moveSpell) {
+      Player.getMovementSpeed(moveSpell);
+      //message handler?
+   }
 }
