357606f390b2e89c1dadea10cf1c30c024dc7246
mj123032 <57683943+mj123032@users.noreply.github.com>
2019-11-26 08:49:51 -0700

    Update Encounters.java

--- Encounters.java
+++ Encounters.java
@@ -24,132 +24,86 @@ public class Encounters {
       
       System.out.println("Encounter Time! ");
       int deciderEncounter = rand.nextInt(6);
+      
+      
 //=================================================First encounter===========================================================
       if(deciderEncounter == 0) {
          System.out.println("Webs!  Casually, you reach up to brush away some webs...");
          System.out.println("But they won't brush away! They cling to you!"); 
          //Choice time
          // System.out.println("(1) Use speed to run out of the webs! (Required speed xx)");
-         System.out.println("(2) Magic! Burn the webs! (Required magic 3)");
+         System.out.println("(2) Magic! Burn the webs!");
          System.out.println("(0) Do nothing. Sucks for you.");
          int choice = input.nextInt();
          System.out.print("What is your choice?: ");
          
-//          if(choice ==1) { //Giving choice to user to choose the outcome. 
-//             if(Player.getSpeed() > 1) {
-//                System.out.println("You were fast enough to break free!");
-//                
-//             }
-//             else {
-//                System.out.println("Oof... you arn't fast enough for that. Gotta take some health now.");
-//                //Player.setPlayerHealth(Player.getPlayerHealth()xx); //Taking heath from player. 
-//             }
-//          }
          if(choice ==2) {
-            if(Player.getMagic() > 2) {
+            if(Application.probabilityCalculator(0.75)) {
                System.out.println(" Kill them with HOLY FIRE! The webs burn");
                   
             }
             else {
-               System.out.println("That feels bad, You're not good enough with your magic to do that. Gotta take some health now.");
-              // Player.setPlayerHealth(Player.getPlayerHealth()xx); //Taking heath from player. 
-               
+               System.out.println("Luck isn't on your side. You fight and fight till you finally get out.");               
             }
          }
          else {
-            System.out.println("due to your incompetents in choosing a valid responce, you lose health.");//IS IT COOL LEAVING THIS? nope.
-            //Player.setPlayerHealth(Player.getPlayerHealth()xx); //Taking heath from player. 
+            System.out.println("Well. I guess we start the next room now.");
          }
-            //Creating a 1second Timer
-            try{
+            try{ //1 second timer. 
                Thread.sleep(1000);//2000ms = 2s
             }catch(InterruptedException ex){}  
       }
 //==================================================Encounter 2====================================================================
       if(deciderEncounter == 1) {
          System.out.println("Something Slimy? What’s around your ankle? A bug? A tentacle? A dead hand crawling!");
-         //Choice time. 
-         //System.out.println("(1) Use speed to run out of the room screaming! (Required speed xx)");//Add the speed and magic require
-         System.out.println("(2) Use Sword to kill the slimy thing! (Require Sword Dmg 3)");
+         //Choice time.
+         System.out.println("(1) Use Sword to kill the slimy thing! (Require Sword Dmg 3)");
          System.out.println("(0) Do nothing. Sucks for you.");
          
          int choice = input.nextInt();
          System.out.print("What is your choice?: ");
-         
-         // if(choice==1) {
-//             if(Player.getSpeed() > 1) {
-//                System.out.println("You Run away screeming like a little girl. But! You get away");
-//                
-//             }
-//             else {
-//                System.out.println("Looks like you didn't make it. Sorry. Minus Hitpoints!");
-//                //Player.setPlayerHealth(Player.getPlayerHealth()xx); //Taking heath from player. 
-//             }
-//          }
-         
-         if(choice==2) {
-            // if(Player.getSwordDmg() > 2) {
-//                System.out.println("You swing... and swing.... and swing... finally it dies");
-//                
-//             }
-           // else {
-               System.out.println("Looks like you didn't make it. Sorry. Minus Hitpoints!");
-               //Player.setPlayerHealth(Player.getPlayerHealth()xx); //Taking heath from player. 
-           // }
+        
+         if(choice==1) {
+            if(Application.probabilityCalculator(0.80)) {
+               System.out.println("You swing... and swing.... and swing... finally it dies");   
+            }
+        else {
+            System.out.println("Looks like the thing still lives. You run away to avoid having it touch you again.");
          }
-         else {
-            System.out.println("due to your incompetents in choosing a valid responce, you lose health.");//IS IT COOL LEAVING THIS?
-            //Player.setPlayerHealth(Player.getPlayerHealth()xx); //Taking heath from player. 
-         } 
-            //Creating a 1 second Timer
-            try{
+    
+            try{//Creating a 1 second Timer
                Thread.sleep(1000);//2000ms = 2s
-            }catch(InterruptedException ex){}  //Yes you need these '{}' don't delete
+            }catch(InterruptedException ex){}
       }
 //==============================================Encounter 3=================================================================
     if(deciderEncounter == 3) {
          System.out.println("Silence. Underground, everything goes silent. Even the sound of breathing has vanished.");
          //Choice time. 
-         System.out.println("(1) Magic remove this wretched curse! (Required Magic 3)");//Add the speed and magic require
-         //System.out.println("(2) Use speed to run out of the room screaming! (Required speed xx)");
-         System.out.println("(3) Explore! Why not? What's the worst that can happen?");
+         System.out.println("(1) Magic remove this wretched curse!");
+         System.out.println("(2) Explore! Why not? What's the worst that can happen?");
          System.out.println("(0) Do nothing. Sucks for you.");
          
-         int choice = input.nextInt();
+         choice = input.nextInt();
          System.out.print("What is your choice?: ");
          
          if(choice==1) {
-            if(Player.getMagic() > 2) {
+            if(Application.probabilityCalculator(0.60)) {
                System.out.println("You you successfully remove the curse! Next Room!");
                
             }
             else {
-               System.out.println("Looks like you didn't make it. Sorry. Minus Hitpoints!");
-               //Player.setPlayerHealth(Player.getPlayerHealth()xx); //Taking heath from player. 
+               System.out.println("Stumbling out of the room you do make it. Just don't go back to that room"); 
             }
          }
-        //  if(choice==2) {
-//             if(Player.getSpeed() > 1) {
-//                System.out.println("You rush out of the room! Suddenly all the sound has returned... Freaky.");
-//                 
-//             }
-//             else {
-//                System.out.println("Looks like you didn't make it. Sorry. Minus Hitpoints!");
-//                //Player.setPlayerHealth(Player.getPlayerHealth()xx); //Taking heath from player. 
-//             }
-//          }
-         if(choice==3) {
-            System.out.println("it’s so quiet you lose your sanity! Ohh Noo!!! -xx health");
-            //Player.setPlayerHealth(Player.getPlayerHealth()xx); //Taking heath from player. 
+         
+         if(choice==2) {
+            System.out.println("it’s so quiet you lose your sanity!");
+            System.out.println("Nothing is found. Nothing is here. It's just quite. ");
          }
-         else {
-            System.out.println("due to your incompetents in choosing a valid responce, you lose health.");//IS IT COOL LEAVING THIS?
-            //Player.setPlayerHealth(Player.getPlayerHealth()xx); //Taking heath from player. 
-         } 
-            //Creating a 1 second Timer
-            try{
+            
+            try{ //Creating a 1 second Timer
                Thread.sleep(1000);//2000ms = 2s
-            }catch(InterruptedException ex){}  //Yes you need these '{}' don't delete
+            }catch(InterruptedException ex){} 
       }
 //=================================================Encounter 4=============================================================
       if(deciderEncounter ==4) {
@@ -159,21 +113,21 @@ public class Encounters {
          System.out.println("(1) Magic, shine some light! (require magic 4");
          System.out.println("(0) Use Sanity!");
          
-         int choice = input.nextInt();
+         choice = input.nextInt();
          System.out.print("What is your choice?: ");
          
          if(choice ==1) {
-            if(Player.getMagic() > 3) {
+            if(Application.probabilityCalculator(0.85)) {
                System.out.println("light emerges from your chest. Go figure that's what would happen.");
                System.out.println("You Can See! You come to the conclusion you were just seeing things");
                
             }
             else {
                System.out.println("Your chest glows slightly....");
-                  //Creating a 2 second Timer
-                  try{
-                     Thread.sleep(2000);//2000ms = 2s
-                  }catch(InterruptedException ex){}  //Yes you need these '{}' don't delete
+                  
+                  try{ //Creating 1 second Timer
+                     Thread.sleep(1000);//2000ms = 2s
+                  }catch(InterruptedException ex){}
                System.out.println("....Creepy");
             }
          }
@@ -187,24 +141,23 @@ public class Encounters {
          System.out.println("They stare at you with cold, dead eyes. The trio swing silently, ");
          System.out.println("then fade into dust that falls to the ground.");
          //Choice Time
-         System.out.println("(1) Magic, Burn!! (Require magic 2)");
+         System.out.println("(1) Magic, Burn!!");
          System.out.println("(0) ");
          
-         int choice = input.nextInt();
+         choice = input.nextInt();
          System.out.print("What is your choice?: ");
          
             if(choice ==1) {
-            if(Player.getMagic() > 2) {
+            if(Application.probabilityCalculator(0.65)) {
                System.out.println("You say \"Yeah... No, Burn the entire thing\" with that, the enire room catches on fire. ");
                System.out.println("");
-               //SINGAL NEXT ROOM
             }
-            //Not finnished. yet. 
             else {
                System.out.println("Really? Nothing? Your soul must be mush to not react to that.");
             }
          }   
-   }//Encounter class closer
+   }//Encounter class closers
 
+}
 }
 }
